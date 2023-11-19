084a93e1901e81a047489dceaa44869e00599b5a
Cesar <cesarvillalobosolmos.01@gmail.com>
2023-11-19 12:46:30 -0600

    Add operations with cart like add, remove, etc. products

--- projects/05-astro-fake-e-commerce/src/components/svelte/CartContainer.svelte
+++ projects/05-astro-fake-e-commerce/src/components/svelte/CartContainer.svelte
@@ -1,5 +1,11 @@
 <script>
-  import { isCartOpen } from "../../store";
+  import {
+    cart,
+    decreaseQuantity,
+    increaseQuantity,
+    isCartOpen,
+    removeProduct,
+  } from "../../store";
 </script>
 
 <div
@@ -14,11 +20,76 @@
     class="flex-1 h-screen bg-gray-500 opacity-50"
   />
 
-  <aside class="w-1/4 h-screen bg-white shadow-lg opacity-100">
-    <ul>
-      <li>
-        <h1>Holis</h1>
-      </li>
-    </ul>
+  <aside class="w-1/4 p-2 h-screen bg-white shadow-lg opacity-100">
+    <h3 class="mb-1 text-lg font-medium text-slate-900">
+      Productos del carrito
+    </h3>
+
+    <h4 class="mb-4 text-4xl text-slate-700 font-thin">
+      Total: ${Object.values($cart).reduce(
+        (acc, el) => acc + el.product.price * el.quantity,
+        0
+      )}
+    </h4>
+
+    <section class="bg-gray-50 p-1 rounded-sm shadow-sm">
+      {#each Object.values($cart) as cartItem}
+        <article class="mb-2 flex flex-row justify-start items-center gap-2">
+          <figure>
+            <img
+              src={cartItem.product.image}
+              alt={cartItem.product.title}
+              class="w-24 aspect-[9/12] object-cover object-center"
+            />
+          </figure>
+
+          <section class="flex-1">
+            <h2 class="mb-2 font-medium">
+              {cartItem.product.title.length > 50
+                ? cartItem.product.title.slice(0, 50) + "..."
+                : cartItem.product.title}
+            </h2>
+
+            <div class="flex flex-row justify-between items-center gap-2">
+              <div class="flex justify-start items-center gap-2">
+                <button
+                  on:click={() => decreaseQuantity(cartItem.product.id)}
+                  class="py-0.5 px-3 shadow bg-slate-700 text-white font-bold rounded-md text-xl"
+                >
+                  -
+                </button>
+
+                <span class="text-slate-900 text-xl">
+                  {cartItem.quantity}
+                </span>
+
+                <button
+                  on:click={() => increaseQuantity(cartItem.product.id)}
+                  class="py-0.5 px-3 shadow bg-slate-700 text-white font-bold rounded-md text-xl"
+                >
+                  +
+                </button>
+              </div>
+
+              <button
+                on:click={() => removeProduct(cartItem.product.id)}
+                class="text-red-600 font-semibold"
+              >
+                Eliminar
+              </button>
+            </div>
+
+            <p class="mt-2 text-gray-600 font-normal">
+              ${cartItem.product.price} -
+              <span class="text-xl font-medium text-slate-800">
+                ${cartItem.quantity * cartItem.product.price}
+              </span>
+            </p>
+          </section>
+        </article>
+      {:else}
+        <h2>No hay productos en el carrito</h2>
+      {/each}
+    </section>
   </aside>
 </div>
--- projects/05-astro-fake-e-commerce/src/store/index.ts
+++ projects/05-astro-fake-e-commerce/src/store/index.ts
@@ -1,8 +1,24 @@
 import { atom, map } from 'nanostores'
 import type { Cart, Product } from '../types/types.d'
 
-export const isCartOpen = atom(false)
-export const cart = map<Record<number, Cart>>({})
+export const isCartOpen = atom(true)
+export const cart = map<Record<number, Cart>>({
+  1: {
+    product: {
+      id: 1,
+      category: 'men',
+      description: 'Slim-fitting style, contrast raglan long sleeve, three-button henley placket, li...',
+      image: 'https://fakestoreapi.com/img/71-3HjGNDUL._AC_SY879._SX._UX._SY._UY_.jpg',
+      price: 100,
+      rating: {
+        count: 100,
+        rate: 5,
+      },
+      title: 'Mens Casual Premium Slim Fit T...',
+    },
+    quantity: 4
+  }
+})
 
 export function addProduct(product: Product) {
   const existsProduct = cart.get()[product.id]
@@ -32,5 +48,29 @@ export function removeProduct(id: number) {
       if (Number(key) === id) return
       cart.setKey(Number(key), product)
     })
+  }
+}
+
+export function increaseQuantity(id: number) {
+  const existsProduct = cart.get()[id]
+
+  if (existsProduct) {
+    cart.setKey(id, {
+      ...existsProduct,
+      quantity: existsProduct.quantity + 1,
+    })
+  }
+}
+
+export function decreaseQuantity(id: number) {
+  const existsProduct = cart.get()[id]
+
+  if (existsProduct) {
+    if (existsProduct.quantity >= 1) {
+      cart.setKey(id, {
+        ...existsProduct,
+        quantity: existsProduct.quantity - 1,
+      })
+    }
   }
 }
