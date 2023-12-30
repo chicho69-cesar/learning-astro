865977dc8ee3a55798a6cb947e9d76f46ddd34dc
Cesar <cesarvillalobosolmos.01@gmail.com>
2023-12-29 23:30:56 -0600

    feat(projects/09-esland-web): Add configurations and dependencies

--- projects/09-esland-web/astro.config.mjs
+++ projects/09-esland-web/astro.config.mjs
@@ -1,4 +1,18 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from 'astro/config'
+
+import tailwind from '@astrojs/tailwind'
+import vercel from '@astrojs/vercel/serverless'
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [tailwind()],
+  i18n: {
+    defaultLocale: 'es',
+    locales: ['es', 'ca'],
+    routing: {
+      prefixDefaultLocale: false
+    }
+  },
+  output: 'server',
+  adapter: vercel()
+})
