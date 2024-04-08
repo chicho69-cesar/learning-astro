f9fe1acaafda9a032f2c89afa2e3dba9a48877a2
Cesar <cesarvillalobosolmos.01@gmail.com>
2024-04-07 23:51:08 -0600

    feat(projects/09-esland-web): Add initial project config

--- projects/09-esland-web/astro.config.mjs
+++ projects/09-esland-web/astro.config.mjs
@@ -2,13 +2,15 @@ import { defineConfig } from 'astro/config'
 
 import tailwind from '@astrojs/tailwind'
 import vercel from '@astrojs/vercel/serverless'
+import preact from '@astrojs/preact'
+import auth from 'auth-astro'
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  integrations: [tailwind(), preact(), auth()],
   i18n: {
     defaultLocale: 'es',
-    locales: ['es', 'ca'],
+    locales: ['es', 'en', 'ca'],
     routing: {
       prefixDefaultLocale: false
     }
--- projects/09-esland-web/tailwind.config.cjs
+++ projects/09-esland-web/tailwind.config.cjs
@@ -4,7 +4,15 @@ import animations from 'tailwindcss-animated'
 export default {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        tomaso: ['Tomaso', 'sans-serif'],
+        jura: ['Jura', 'sans-serif'],
+      },
+      colors: {
+        primary: 'rgb(253 224 71);',
+      },
+    },
   },
   plugins: [
     animations,
@@ -13,7 +21,7 @@ export default {
         '.cp-v': {
           clipPath: 'polygon(0 0, 100% 0, 100% 85%, 50% 100%, 50% 100%, 0 85%)',
         },
-      });
+      })
     },
   ],
 }
