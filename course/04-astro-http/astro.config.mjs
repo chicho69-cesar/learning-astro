8aae3f6eb4d45ff120a10d66de75a3420af1ecdb
Cesar <cesarvillalobosolmos.01@gmail.com>
2024-08-31 22:25:52 -0600

    feat(Root): Update config files for astro http project

--- course/04-astro-http/astro.config.mjs
+++ course/04-astro-http/astro.config.mjs
@@ -1,9 +1,22 @@
-import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
+import { defineConfig } from 'astro/config';
+// import node from '@astrojs/node';
+import cloudflare from '@astrojs/cloudflare';
+import db from '@astrojs/db';
+import vue from '@astrojs/vue';
 
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://example.com',
-	integrations: [mdx(), sitemap()],
+	integrations: [mdx(), sitemap(), db(), vue()],
+	output: 'hybrid',
+	// adapter: node({
+	// 	mode: 'standalone'
+	// })
+	// output: 'server',
+  adapter: cloudflare(),
+	experimental: {
+		actions: true
+	}
 });
