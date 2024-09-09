4a14403885331ed96aad6e8eeb4945b54af64632
Cesar <cesarvillalobosolmos.01@gmail.com>
2024-09-08 23:19:03 -0600

    feat(Root): Add and declare project types

--- course/06-astro-store/src/env.d.ts
+++ course/06-astro-store/src/env.d.ts
@@ -1,1 +1,19 @@
+/// <reference path="../.astro/db-types.d.ts" />
 /// <reference path="../.astro/types.d.ts" />
+/// <reference path="../.astro/actions.d.ts" />
+/// <reference types="astro/client" />
+
+interface User {
+  email: string;
+  name: string;
+  // avatar: string;
+  // emailVerified: boolean;
+}
+
+declare namespace App {
+  interface Locals {
+    isLoggedIn: boolean;
+    isAdmin: boolean;
+    user: User | null;
+  }
+}
