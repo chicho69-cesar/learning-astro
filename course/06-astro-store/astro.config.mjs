ebb2fd44b4ab54ca4f2421a9f7d2c3e5b30a22e6
Cesar <cesarvillalobosolmos.01@gmail.com>
2024-09-10 17:49:35 -0600

    chore(Root): Update project settings

--- course/06-astro-store/astro.config.mjs
+++ course/06-astro-store/astro.config.mjs
@@ -3,10 +3,11 @@ import tailwind from '@astrojs/tailwind';
 import netlify from '@astrojs/netlify';
 import db from '@astrojs/db';
 import auth from 'auth-astro';
+import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), db(), auth()],
+  integrations: [tailwind(), db(), auth(), react()],
   output: 'server',
   adapter: netlify(),
   experimental: {
