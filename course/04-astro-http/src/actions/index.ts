319324d9c090b8fecaeff0811ce94903039579da
Cesar <cesarvillalobosolmos.01@gmail.com>
2025-01-02 12:13:43 -0600

    fix(Root): Remove astro http to separate in sections

--- course/04-astro-http/src/actions/index.ts
+++
@@ -1,8 +0,0 @@
-import { getPostLikes } from './posts/get-post-likes.action';
-import { updatePostsLikes } from './posts/update-post-likes.action';
-
-export const server = {
-  // Posts
-  getPostLikes,
-  updatePostsLikes,
-};
